f8dd07f989c317922ed39219def688ff64ea8989
kiki <kikiwang3185@gmail.com>
2023-07-07 16:11:10 +0800

    A SIMPLE APP

--- week2/Remote_Assignment_Week2/Remote_Assignment_Week2/Model/File.swift
+++ week2/Remote_Assignment_Week2/Remote_Assignment_Week2/Model/File.swift
@@ -6,13 +6,14 @@
 //
 
 import Foundation
+import UIKit
 
-let text = [
-"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus.",
-"Contrary to popular belief, Lorem Ipsum is not simply random text.",
-"Richard McClintock, a Latin professor at Hampden-Sydney College in ",
-"looked up one of the more obscure Latin words, consectetur",
-"from a Lorem Ipsum passage, and going through the cities of the word",
-"This book is a treatise on the theory of ethics, very popular during the.",
-"The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.."
+let colorPairs: [colorPair] = [
+    colorPair(color: UIColor(red: 1, green: 0.68, blue: 0.74, alpha: 1), text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus."),
+    colorPair(color: UIColor(red: 1.00, green: 0.64, blue: 0.52, alpha: 1.00), text: "Contrary to popular belief, Lorem Ipsum is not simply random text."),
+    colorPair(color: UIColor(red: 0.84, green: 0.68, blue: 0.38, alpha: 1.00), text: "Richard McClintock, a Latin professor at Hampden-Sydney College in "),
+    colorPair(color: UIColor(red: 0.35, green: 0.60, blue: 0.10, alpha: 1.00), text: "looked up one of the more obscure Latin words, consectetur"),
+    colorPair(color: UIColor(red: 0.49, green: 0.78, blue: 0.89, alpha: 1.00), text: "from a Lorem Ipsum passage, and going through the cities of the word"),
+    colorPair(color: UIColor(red: 0.08, green: 0.36, blue: 0.63, alpha: 1.00), text: "This book is a treatise on the theory of ethics, very popular during the."),
+    colorPair(color: UIColor(red: 0.66, green: 0.53, blue: 0.63, alpha: 1.00), text: "The first line of Lorem Ipsum, Lorem ipsum dolor sit amet..")
 ]
