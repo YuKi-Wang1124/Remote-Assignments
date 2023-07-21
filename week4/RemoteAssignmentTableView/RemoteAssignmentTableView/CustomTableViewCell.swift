a1c7ee3250e9d1e08d02770f852d3ec8b0d5802b
kiki <kikiwang3185@gmail.com>
2023-07-21 19:00:22 +0800

    add URLSession remote assignment

--- week4/RemoteAssignmentTableView/RemoteAssignmentTableView/CustomTableViewCell.swift
+++ week4/RemoteAssignmentTableView/RemoteAssignmentTableView/CustomTableViewCell.swift
@@ -18,6 +18,7 @@ class CustomTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.isUserInteractionEnabled = false
         contentView.backgroundColor = .white
         contentView.addSubview(label)
     }
---
+++ week4/Remote_Assignmennt_URLSession/Remote_Assignmennt_URLSession/StationInfo.swift
@@ -0,0 +1,14 @@
+//
+//  stationInfo.swift
+//  Remote_Assignmennt_URLSession
+//
+//  Created by 王昱淇 on 2023/7/21.
+//
+
+import Foundation
+
+struct StationInfo: Codable {
+    let stationID: String
+    let stationName: String
+    let stationAddress: String
+}
